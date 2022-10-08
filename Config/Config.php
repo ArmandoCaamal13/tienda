9090f3b68335e1fba47459a17820501528b623f7
Erubiel Tuk Balam <tukerubalamutlook.com>
2022-10-07 23:29:57 -0500

    subiendo los primeros archivos

---
+++ Config/Config.php
@@ -0,0 +1,15 @@
+<?php
+const BASE_URL = "http://localhost/tienda-virtual/";
+const HOST = "localhost";
+const USER = "root";
+const PASS = "";
+const DB = "tienda_virtual";
+const CHARSET = "charset=utf8";
+const TITLE = "TIENDA ONLINE";
+const MONEDA = "MXM";
+const CLIENT_ID = "AZlRFeSXl3mfYytrVLkzXK0FgTBW7s_Ok21atTAEjJe5uM-0pRKe729Zb_K7kQkhHqJv2z041UrWK9wl";
+const USER_SMTP = "erubieltuk@gmail.com";
+const PASS_SMTP = "cmhdypvjpkbolaxx";
+const PUERTO_SMTP = 465;
+const HOST_SMTP = "smtp.gmail.com";
+?>
---
+++ assets/js/clientes.js
@@ -0,0 +1,66 @@
+const tableLista = document.querySelector('#tableListaProductos tbody');
+document.addEventListener('DOMContentLoaded', function() {
+  if (tableLista) {
+    getListaProductos();
+  }
+});
+
+function getListaProductos() {
+    const url = base_url + "principal/listaProductos";
+    const http = new XMLHttpRequest();
+    http.open('POST', url, true);
+    http.send(JSON.stringify(listaCarrito));
+    http.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        const res = JSON.parse(this.responseText);
+        let html = '';
+        res.productos.forEach(producto => {
+          html += `<tr>
+                <td>
+                <img class="img-thumbnail rounded-circle" src="${producto.imagen}" alt="" width="100">
+                </td>
+                <td>${producto.nombre}</td>
+                <td><span class="badge bg-warning">${res.moneda + ' ' + producto.precio}</span></td>
+                <td><span class="badge bg-primary">${ producto.cantidad}</span></td>
+                <td>${producto.subTotal}</td>
+            </tr>`;
+        });
+        tableLista.innerHTML = html;
+        document.querySelector('#totalProducto').textContent = 'TOTAL A PAGAR: ' + res.moneda + ' ' + res.total;
+        botonPaypal(res.totalPaypal);
+      }
+    }
+  }
+
+  function botonPaypal(total) {
+    paypal.Buttons({
+      // Sets up the transaction when a payment button is clicked
+      createOrder: (data, actions) => {
+        return actions.order.create({
+          purchase_units: [{
+            amount: {
+              value: total // Can also reference a variable or function
+            }
+          }]
+        });
+      },
+      // Finalize the transaction after payer approval
+      onApprove: (data, actions) => {
+        return actions.order.capture().then(function(orderData) {
+          registrarPedido(orderData)
+        });
+      }
+    }).render('#paypal-button-container');
+  }
+  function registrarPedido(datos) {
+    const url = base_url + "clientes/registrarPedido";
+    const http = new XMLHttpRequest();
+    http.open('POST', url, true);
+    http.send(JSON.stringify(datos));
+    http.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        console.log(this.responseText);
+        //const res = JSON.parse(this.responseText);
+      }
+    }
+  }
